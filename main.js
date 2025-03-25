97fb724b096a2679a3e499d599421fda416b89a1
David Reichardt <davereichardt@gmail.com>
2025-03-25 17:58:01 -0400

    has basic functionality, updates display

--- main.js
+++ main.js
@@ -9,12 +9,24 @@ const equal = document.getElementById('equal');
 const decimal = document.getElementById('decimal');
 const backspace = document.getElementById('backspace');
 const negative = document.getElementById('negative');
+const resultDisplay = document.getElementById('result');
+const number1Display = document.getElementById('number1');
+const number2Display = document.getElementById('number2');
+const operatorDisplay = document.getElementById('operator');
+
+function updateDisplay() {
+  number1Display.textContent = number1;
+  number2Display.textContent = number2;
+  operatorDisplay.textContent = operator;
+  resultDisplay.textContent = result;
+}
 
 clearButton.addEventListener('click', function () {
   number1 = '';
   number2 = '';
   operator = '';
   result = '';
+  updateDisplay();
 });
 
 equal.addEventListener('click', function () {
@@ -32,14 +44,19 @@ equal.addEventListener('click', function () {
     case '/':
       result = parseInt(number1) / parseInt(number2);
   }
+  updateDisplay();
 });
 
 numbers.forEach((number) => {
   number.addEventListener('click', (e) => {
     if (operator === '') {
       number1 += e.target.textContent;
+      result += e.target.textContent;
+      updateDisplay();
     } else {
       number2 += e.target.textContent;
+      result += e.target.textContent;
+      updateDisplay();
     }
   });
 });
@@ -47,5 +64,7 @@ numbers.forEach((number) => {
 operators.forEach((op) => {
   op.addEventListener('click', (e) => {
     operator = e.target.textContent;
+      result += e.target.textContent;
+      updateDisplay();
   });
 });
